86f3f1ab5f1634c0d340f7c4923360f62eb494fa
Saleem Ashrafi <ashrafis@PHARMEVO.COM>
2017-09-29 16:20:59 +0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+ReactDOM.render(<h1> Hello Wold! </h1>, document.getElementById('root'));
+registerServiceWorker();
